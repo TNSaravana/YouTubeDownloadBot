0680a544b06aeeabb948773db66de67712ff00c0
TNSaravana <163824886+TNSaravana@users.noreply.github.com>
2024-03-19 14:13:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/AnyDLBot.git /AnyDLBot
-cd /AnyDLBot
+git clone https://github.com/TNSaravana/YouTubeDownloadBot.git /YouTubeDownloadBot
+cd /YouTubeDownloadBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
